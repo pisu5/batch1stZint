c2ca1cd30a321032498505803dc73861ef184950
lenovo <prai47465@gmail.com>
2024-07-08 10:48:56 +0530

    if-else-questions solved 8

---
+++ javascript/if-else-questions.js
@@ -0,0 +1,117 @@
+//que 1
+
+/*
+let a = -2;
+if (a > 0) {
+  console.log("Number is positive");
+} else if (a === 0) {
+  console.log("Num is zero");
+} else {
+  console.log("Num is negative");
+}
+  
+
+//que 2
+let char = "A";
+if (
+  char === "a" ||
+  char === "e" ||
+  char === "i" ||
+  char === "o" ||
+  char === "u"
+) {
+  console.log("Char is vowel");
+} else if (
+  char === "A" ||
+  char === "E" ||
+  char === "I" ||
+  char === "O" ||
+  char === "U"
+) {
+  console.log("Char is vowel");
+} else if ((char >= "A" && char <= "Z") || (char >= "a" && char <= "z")) {
+  console.log("char is consonent");
+} else {
+  console.log("invalid character");
+}
+  
+
+//QUE 3
+
+let char = "*";
+if (char >= "A" && char <= "Z") {
+    console.log("Letter is uppercase")
+}else if((char >= "a" && char <= "z")){
+    console.log("Letter is lowercase");
+}else{
+    console.log("Invalid character");
+}
+    
+
+//que 4
+let a = 100;
+let b = 50;
+let c = 30;
+let valid = a + b + c;
+if (valid === 180) {
+  console.log("Truangle is valid");
+} else {
+  console.log("Triangle is not valid");
+}
+  
+
+//QUE NO 6 using nested if apro 1
+let a = 100;
+let b = 50;
+let c = 100;
+if (a === b) {
+  //outer if conditions
+  if (b === c) {
+    //inner if cnditions
+    console.log("This is Equli triangle");
+  } else {
+    console.log("This is Isco triangle");
+  }
+} else if (a === c) {
+  console.log("This is iso traingle");
+} else if (b === c) {
+  console.log("This is iso traingle");
+} else {
+  console.log("This is scelene triangle");
+}
+  
+
+//QUE 6 appro 2
+
+
+let a = 100;
+let b = 100;
+let c = 100;
+if(a===b && b===c){
+    console.log("Equli triangle")
+}else if(a===b || b===c || a===c){
+    console.log("This is iso");
+}else{
+    console.log("Scelene trinagle")
+}
+    */
+
+//QUE 8TH
+
+let ram = 20;
+let shyam = 20;
+let ajay = 20;
+
+if (ram < shyam) {
+  //outer
+  if (ram < ajay) {
+    //inner
+    console.log("Ram is Youngest banda");
+  } else {
+    console.log("ajay is young");
+  }
+} else if (shyam < ajay) {
+  console.log("shyam");
+} else {
+  console.log("Ajay is young");
+}
