3b9fbe7de1d607ae739e4ca020653c15b7fd7d1f
lenovo <prai47465@gmail.com>
2024-07-11 10:45:16 +0530

    addForLoop

--- javascript/forloop.js
+++ javascript/forloop.js
@@ -12,7 +12,7 @@ for (let i = 0; i <= num; i++) {
     console.log(i);
   }
 }
-  */
+  //que 3
 const series = 7;
 let firstNum = 0;
 let seconNum = 1;
@@ -22,4 +22,47 @@ for (let i = 2; i <= series; i++) {
 
   firstNum = seconNum;
   seconNum = nextNum;
+}
+  
+//que 4
+let a = 3;
+let result = 1;
+for (let i = 1; i <= a; i++) {
+  result *= i;
+}
+console.log(result)
+
+
+//que 6
+let a = 8;
+for(let i =1;i<=10;i++){
+  console.log(a*i)
+
+}
+  
+//que 7
+console.log("svsrge");
+const num = 3;
+let isprime = true;
+for (let i = 2; i < num; i++) {
+  if (num % i === 0) {
+    isprime = false;
+    break;
+  }
+}
+console.log(isprime);
+*/
+//que 8 print all prime 
+let m = 10;
+for (let i = 2; i <= m; i++) { //outer loop 
+  let isPrime = true;
+  for (let j = 2; j < i; j++) { //inner loop
+    if (i % j == 0) {
+      isPrime = false;
+      break;
+    }
+  }
+  if (isPrime) {
+    console.log(i);
+  }
 }
--- javascript/ifelse.js
+++
@@ -1,7 +0,0 @@
-//que 1
-const a =8;
-if(a%2==0){
-    console.log("This is even")
-}else{
-    console.log("odd")
-}
