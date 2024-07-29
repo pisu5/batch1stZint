2de0840b8f92a807ac77d899f066818fe9571d82
lenovo <prai47465@gmail.com>
2024-07-29 14:05:50 +0530

    zomatoprogress

---
+++ java/Matrix2d.java
@@ -0,0 +1,69 @@
+public class Matrix2d {
+    public static void main(String[] args) {
+        /*
+         * 2d array declare
+         * int[][] declare = new int[3][4];
+         * declare[0] = new int[] { 8, 8, 8, 8 };
+         * declare[1] = new int[] { 8, 8, 8, 8 };
+         * declare[2] = new int[] { 8, 8, 8, 8 };
+         * 
+         * 
+         * // 2d array initilize
+         * int[][] arr = { { 1, 2, 4 }, { 7, 8 } };
+         * // int value = arr[1][2];
+         * for (int i = 0; i < arr.length; i++) {
+         * int temp = arr[i].length;
+         * System.out.println(i + " " + " " + temp);
+         * 
+         * }
+         * 
+         * 
+         * // sum of rows
+         * int[][] arr = { { 1, 2, 4 }, { 7, 8 } };
+         * for (int i = 0; i < arr.length; i++) {
+         * int rowslen = arr[i].length;
+         * int sum = 0;
+         * for (int j = 0; j < rowslen; j++) {
+         * sum =sum+ arr[i][j];
+         * }
+         * System.out.println("Sum of row " + " " + i + " " + sum);
+         * }
+         
+        //que no-7
+        int[][] mat = { { 1, 2, 3, 4, 5 }, { 6, 7, 8, 9 }, { 10, 11, 12, 13, 14, 15 } };
+        int max = mat[0][0];
+        for (int rows = 0; rows < mat.length; rows++) {
+
+            int rowsLength = mat[rows].length;
+            for (int columns = 0; columns < rowsLength; columns++) {
+                if (mat[rows][columns] > max) {
+                    max = mat[rows][columns];
+                }
+
+            }
+
+        }
+        System.out.println(max);
+        */
+        // que no-9
+        int[][] mat = { { 1, 2, 3, 5 }, { 6, 7, 8, 9 }, { 10, 13, 14, 15 } };
+        int[][] mat2 = { { 1, 2, 3, 5 }, { 6, 7, 8, 9 }, { 10,  13, 14, 15 } };
+        boolean flag = true;
+        if(mat.length==mat2.length && mat[0].length==mat2[0].length){
+            for(int i = 0;i<mat.length;i++){
+                for(int j = 0;j<mat[0].length;j++){
+                    if(mat[i][j]!=mat2[i][j]){
+                        flag = false;
+                    }
+
+
+                }
+            }
+        }else{
+            flag = false;
+        }
+        System.out.println(flag);
+
+    }
+
+}
---
+++ javascript/setTimeout.js
@@ -0,0 +1,8 @@
+function greet() {
+  console.log("game is over");
+}
+//console.log("Game is start :");
+//setTimeout(greet, 4000);
+setTimeout(() => {
+  console.log("Time is start...");
+}, 3000);
