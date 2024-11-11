ba56e6753b154f4f5d34dafddd786e1956987000
lenovo <prai47465@gmail.com>
2024-11-11 12:57:05 +0530

    addReact

--- afterNoonbatch/ReactStart/ReactFirstApp/src/List.jsx
+++ afterNoonbatch/ReactStart/ReactFirstApp/src/List.jsx
@@ -7,9 +7,60 @@ const List = () => {
     { id: 2, name: "arnav", Position: "CEO", salary: "700000" },
   ];
 
+  const Phonedata = [
+    {
+      id: 0,
+      name: "PoCO M6 5G",
+      url: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/9/b/n/-original-imah3afnqj84usyy.jpeg?q=70",
+      price: "₹7,999",
+    },
+    {
+      id: 1,
+      name: "Vivo T3 5G",
+      url: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/w/3/4/-original-imahyytukhkky5ew.jpeg?q=70",
+      price: "₹12,999",
+    },
+    {
+      id: 2,
+      name: "realme 12 5G",
+      url: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/w/c/3/-original-imah3chxfqnezjjb.jpeg?q=70",
+      price: "₹12,999",
+    },
+    {
+      id: 3,
+      name: "Apple iPhone 15",
+      url: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/k/l/l/-original-imagtc5fz9spysyk.jpeg?q=70",
+      price: "₹58,999",
+    },
+    {
+      id: 4,
+      name: "PoCO M6 5G",
+      url: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/9/b/n/-original-imah3afnqj84usyy.jpeg?q=70",
+      price: "₹7,999",
+    },
+    {
+      id: 5,
+      name: "Vivo T3 5G",
+      url: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/w/3/4/-original-imahyytukhkky5ew.jpeg?q=70",
+      price: "₹12,999",
+    },
+    {
+      id: 6,
+      name: "realme 12 5G",
+      url: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/w/c/3/-original-imah3chxfqnezjjb.jpeg?q=70",
+      price: "₹12,999",
+    },
+    {
+      id: 7,
+      name: "Apple iPhone 15",
+      url: "https://rukminim2.flixcart.com/image/312/312/xif0q/mobile/k/l/l/-original-imagtc5fz9spysyk.jpeg?q=70",
+      price: "₹58,999",
+    },
+  ];
+
   return (
     <div>
-      <ul>
+      {/** <ul>
         {Fruits.map((value, index) => (
           <h1>{`${index}${value}`}</h1>
         ))}
@@ -34,7 +85,23 @@ const List = () => {
             <td>{value.salary}</td>
           </tr>
         ))}
-      </table>
+      </table> */}
+
+      <div className="phones-list">
+        <div className="phone-head">
+          <h1>View SmartPhones</h1>
+          <button>View All</button>
+        </div>
+        <div className="list">
+          {Phonedata.map((value) => (
+            <div key={value.id} className="phones">
+              <img src={value.url} alt="" />
+              <h3>{value.name}</h3>
+              <p>{value.price}</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- java/batch1/java/altab/Matricks.java
+++ java/batch1/java/altab/Matricks.java
@@ -2,22 +2,47 @@ package batch1.java.altab;
 
 public class Matricks {
     public static void main(String[] args) {
-        int mat[][] = new int[2][3]; // declare
-        mat[0][0] = 45;
-        mat[0][1] = 34;
-        mat[0][2] = 55;
+        /*
+         * int mat[][] = new int[2][3]; // declare
+         * mat[0][0] = 45;
+         * mat[0][1] = 34;
+         * mat[0][2] = 55;
+         * 
+         * mat[1][0] = 45;
+         * mat[1][1] = 34;
+         * mat[1][2] = 55;
+         * int sum = 0;
+         * 
+         * for (int i = 0; i < mat.length; i++) {
+         * for (int j = 0; j < mat[0].length; j++) {
+         * sum +=mat[i][j];
+         * System.out.println(mat[i][j]);
+         * }
+         * }
+         *  // que 10
 
-        mat[1][0] = 45;
-        mat[1][1] = 34;
-        mat[1][2] = 55;
-        int sum  = 0;
+        int[][] ori = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+        int[][] copy = new int[ori.length][ori[0].length];
+        for(int i =0;i<ori.length;i++){
+            for(int j =0;j<ori[0].length;j++){
+                copy[i][j] = ori[i][j];
+            }
+            
+        }
 
-        for (int i = 0; i < mat.length; i++) {
-            for (int j = 0; j < mat[0].length; j++) {
-                sum +=mat[i][j];
-                System.out.println(mat[i][j]);
+         */
+        //que 11
+        int[][] ori = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+        int rows = ori.length;
+        int col = ori[0].length;
+        for(int j =0;j<col;j++){
+            for(int i =0;i<rows;i++){
+                System.out.println(ori[i][j]);
             }
         }
 
+
+
+       
     }
 }
